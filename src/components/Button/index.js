858060a20c2f82ea9a7e46dc364ebe0a572bd835
John Robinson <johnarobinson133@gmail.com>
2021-02-09 11:45:27 -0700

    added style for navbar and links for navbar

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -13,6 +13,7 @@ export const Button =({
     buttonStyle,
     buttonSize
 }) => {
+    
     const checkButtonStyle = STYLES.includes(buttonStyle) ? buttonStyle : STYLES[0]
     const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize : SIZES[0]
 
@@ -22,5 +23,5 @@ export const Button =({
                 {children}
             </button>
         </Link>
-    )
+    );
 };
