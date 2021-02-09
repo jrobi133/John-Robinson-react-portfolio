85d54bc50f288b08695ee14be82db3a637e40be9
John Robinson <johnarobinson133@gmail.com>
2021-02-09 12:58:22 -0700

    added contact.js, portfolio.js, and sighup.js pages

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import './App.css';
 import Navbar from './components/Navbar';
 import {BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './pages/Home';
+import Portfolio from './pages/Portfolio';
+import Contact from './pages/Contact';
+import SignUp from './pages/SignUp';
 
 function App() {
   return (
@@ -11,6 +14,9 @@ function App() {
       <Navbar />
       <Switch>
       <Route exact path="/" component={Home} />
+      <Route exact path="/portfolio" component={Portfolio} />
+      <Route exact path="/contact" component={Contact} />
+      <Route exact path="/sign-up" component={SignUp} />
       </Switch>
     </Router>
     </>
