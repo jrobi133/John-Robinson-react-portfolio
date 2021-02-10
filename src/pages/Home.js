1c431bcf069a795ec5d0fd5e5cbaadb021608fff
John Robinson <johnarobinson133@gmail.com>
2021-02-10 13:27:08 -0700

    added tools and toolsItem components

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,11 +2,13 @@ import React from 'react'
 import '../App.css';
 import Footer from '../components/Footer';
 import HeroSection from '../components/HeroSeaction';
+import Tools from '../components/Tools';
 
 function Home () {
     return (
         <>
         <HeroSection />
+        <Tools />
         <Footer />
         </>
     )
