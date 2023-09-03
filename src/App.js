cf3c467644afb95a6e38d8d215bce232c7476b0a
John Robinson <johnarobinson133@gmail.com>
2023-09-02 19:13:44 -0600

    updated out of date components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import React from 'react';
-import './App.css';
-import Navbar from './components/Navbar';
-import {BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import Portfolio from './pages/Portfolio';
-import Contact from './pages/Contact';
-import SignUp from './pages/SignUp';
+import React from "react";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Portfolio from "./pages/Portfolio";
+import Contact from "./pages/Contact";
+import SignUp from "./pages/SignUp";
 
 function App() {
   return (
     <>
-    <Router>
-      <Navbar />
-      <Switch>
-      <Route exact path="/" component={Home} />
-      <Route exact path="/portfolio" component={Portfolio} />
-      <Route exact path="/contact" component={Contact} />
-      <Route exact path="/sign-up" component={SignUp} />
-      </Switch>
-    </Router>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/portfolio" element={<Portfolio />} />
+          <Route exact path="/contact" element={<Contact />} />
+          <Route exact path="/sign-up" element={<SignUp />} />
+        </Routes>
+      </Router>
     </>
   );
 }
